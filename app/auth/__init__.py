a57095e23cb5073628952e3698ccbc4ee405ffb0
ivansoriasolis <soria.solis.ivan@googlemail.com>
2023-02-17 19:11:47 -0500

    con blueprints

---
+++ app/auth/__init__.py
@@ -0,0 +1,4 @@
+from flask import Blueprint
+auth_bp = Blueprint('auth', __name__, template_folder='templates')
+
+from . import routes
---
+++ app/models.py
@@ -0,0 +1,43 @@
+from flask import url_for
+from slugify import slugify
+from sqlalchemy.exc import IntegrityError
+
+from app import db
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('blog_user.id', ondelete='CASCADE'), nullable=False)
+    title = db.Column(db.String(256), nullable=False)
+    title_slug = db.Column(db.String(256), unique=True, nullable=False)
+    content = db.Column(db.Text)
+
+    def __repr__(self):
+        return f'<Post {self.title}>'
+
+    def save(self):
+        if not self.id:
+            db.session.add(self)
+        if not self.title_slug:
+            self.title_slug = slugify(self.title)
+
+        saved = False
+        count = 0
+        while not saved:
+            try:
+                db.session.commit()
+                saved = True
+            except IntegrityError:
+                count += 1
+                self.title_slug = f'{self.title_slug}-{count}'
+
+    def public_url(self):
+        return url_for('public.show_post', slug=self.title_slug)
+
+    @staticmethod
+    def get_by_slug(slug):
+        return Post.query.filter_by(title_slug=slug).first()
+
+    @staticmethod
+    def get_all():
+        return Post.query.all()
